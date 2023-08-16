c2eb9f92dea3596f5cc813549313d8be00029ae6
sergeisporyshev <sergei.sporyshev@gmail.com>
2023-08-16 18:45:57 +0800

    s3 website hosting s3 cors [yandex cloud]

--- core/interfaces/yc/buckets.ts
+++ core/interfaces/yc/buckets.ts
@@ -2,11 +2,37 @@ export interface BucketVersioning{
     enabled: boolean;
 }
 
+export interface BucketWebSiteParams{
+    index?: string;
+    error?: string;
+}
+
+export interface BucketWebSite{
+    enabled: boolean;
+    params?: BucketWebSiteParams;
+}
+
+export interface BucketCorsParams{
+    allowedHeaders?: string[];
+    allowedMethods?: string[];
+    allowedOrigins?: string[];
+    exposeHeaders?: string[];
+    maxAgeSeconds?: number;
+}
+
+export interface BucketCors{
+    enabled: boolean;
+    params?: BucketCorsParams;
+}
+
 export interface Bucket{
     name: string;
-    acl: string;
-    defaultStorageClass: string;
-    versioning: BucketVersioning
+
+    acl?: string;
+    defaultStorageClass?: string;
+    versioning?: BucketVersioning;
+    website?: BucketWebSite;
+    cors?: BucketCors;
 }
 
 export interface BucketsOutput{
--- modules/yc/buckets.ts
+++ modules/yc/buckets.ts
@@ -16,19 +16,46 @@ export class Buckets extends Construct{
     ) {
         super(scope, name);
 
+        const __defaults = {
+            acl: "private",
+            defaultStorageClass: "STANDARD",
+            website: {
+                index: 'index.html',
+                error: 'error.html'
+            },
+            cors: {
+                allowedHeaders: ['*'],
+                allowedOrigins: ['*'],
+                allowedMethods: ['GET']
+            }
+        }
+
         buckets.forEach((item: Bucket) => {
             const _bId = item.name;
             this.buckets[_bId] = new StorageBucket(
                 scope,
                 _bId, {
                     bucket: item.name,
-                    acl: item.acl,
-                    defaultStorageClass: item.defaultStorageClass,
-                    versioning: {
+                    acl: item.acl ? item.acl : __defaults.acl,
+                    defaultStorageClass: item.defaultStorageClass ? item.defaultStorageClass : __defaults.defaultStorageClass,
+                    versioning: item.versioning && item.versioning.enabled ? {
                         enabled: item.versioning.enabled
-                    },
+                    } : undefined,
                     accessKey: accessKey,
-                    secretKey: secretKey
+                    secretKey: secretKey,
+
+                    website: item.website && item.website.enabled ? {
+                        indexDocument: item.website.params && item.website.params.index ? item.website.params.index : __defaults.website.index,
+                        errorDocument: item.website.params && item.website.params.error ? item.website.params.error : __defaults.website.error
+                    } : undefined,
+
+                    corsRule: item.cors && item.cors.enabled ? [{
+                        allowedOrigins: item.cors.params && item.cors.params.allowedOrigins ? item.cors.params.allowedOrigins : __defaults.cors.allowedOrigins,
+                        allowedHeaders: item.cors.params && item.cors.params.allowedHeaders ? item.cors.params.allowedHeaders : __defaults.cors.allowedHeaders,
+                        allowedMethods: item.cors.params && item.cors.params.allowedMethods ? item.cors.params.allowedMethods : __defaults.cors.allowedMethods,
+                        exposeHeaders: item.cors.params && item.cors.params.exposeHeaders ? item.cors.params.exposeHeaders : undefined,
+                        maxAgeSeconds: item.cors.params && item.cors.params.maxAgeSeconds ? item.cors.params.maxAgeSeconds : undefined
+                    }] : undefined
                 }
             )
         });
